3aec3bdad474d5b92bb6cfff829906d69f36f44f
Danyelle Angelo <danyangelofsa@gmail.com>
2021-05-25 22:51:03 -0300

    Arquivos atualizados, mas não monitorados

--- Implementation/rmm-tree-classic/rmm_tree_classic_v2.cpp
+++ Implementation/rmm-tree-classic/rmm_tree_classic_v2.cpp
@@ -59,71 +59,6 @@ class RMMTree{
 		s-=1;
 		return ((2*b_rank(e))-e) - ((2*b_rank(s))-s);
 	}
-	int leafInTree(int k){
-		int t = pow(2,ceil(log2(numberLeaves)));
-
-		if(k <= (2*numberLeaves) - t)return t-2+k;
-		else return t-2-numberLeaves+k;
-	}
-
-	/*Procura o excesso no bloco*/
-	int fwdBlock(int i,int d,int *dr){
-		int subBlock = ceil((double)(i+1)/w);
-		int t = ceil((double)(i+1)/sizeBlock)*(sizeBlock/w);
-		int x,p;
-		*dr=0;
-
-		//a ideia é varrer os w bits (a partir de i+1) do subbloco de bv.
-		for(int j=i+1;j<=subBlock*w;j++){
-			*dr+= (bv[j]==1) ? 1 : -1;
-			if(*dr== d)return j;
-		}
-		//se não encontrarmos d no pequeno bloco, precisamos varrer o restante do grande bloco a qual i pertence
-		
-		for(p=subBlock+1;p<=t;p++){
-			x = bitsread((p-1)*w,(p*w)-1);
-			if(*dr + tableC[x].excessMin <= d)break;//d está no grande bloco de i
-			*dr += tableC[x].excess;
-		}
-		if(p > t)return (t*w)+1;//d não está nesse bloco, retorna o índice do primeiro elemento do próximo bloco
-		
-		//d está no grande bloco restante (depois de i+1+w) de i
-		for(int j=((p-1)*w)+1;j<p*w;j++){
-			*dr += (bv[j]==1) ? 1 : -1;
-			if(*dr== d)return j;
-		}
-		return 0;
-	}
-
-	int fwdSearch(int i,int d){
-		int dr,j,v;
-		//verifica primeiro se d está no bloco a qual pertence "i"
-		j = fwdBlock(i,d,&dr);
-		if(dr == d) return j;
-
-		//caso não esteja, inicia a subida na árvore
-		v = leafInTree(i);
-		
-		//verifica se é uma folha esquerda e se o excesso relativo está no intervalo do elemento à esquerda 
-		while(v>=1 && dr + tree[v+1].excessMin > d){
-			if(v%2!=0)dr -= tree[v+1].excess;
-			v = ceil((double)v/2);
-		}
-		//TODO verifica se achou nos super-intervalos
-
-	
-		//a ideia é descer na árvore até que cheguemos nas folhas, quando chegarmos lá, escaneamos o seu bloco
-		while(v<numberLeaves){
-			if(dr+tree[(2*v)+1].excessMin<=d)v=(2*v)+1;//excesso está no filho a esquerda
-			else{//excesso está no filho a direita
-				dr += tree[(2*v)+1].excess;
-				v = (2*v)+2;
-			}
-		}
-		int k =((v-1)*sizeBlock) +1; //indice onde a folha começa
-		j = fwdBlock(k, d-dr, &dr);
-		return j;
-	}
 
 	void buildingTree(){
 		/*pré-computa tabela C, para acelerar a cosntruçã da RMM-tree*/
@@ -134,7 +69,7 @@ class RMMTree{
 		int prevLevelLeaves = numberLeaves - lastLevelLeaves;
 		int kth;
 
-		if(1 <= lastLevelLeaves)kth = pow(2,height)-1; 
+		if(1 <= lastLevelLeaves)kth = pow(2,height)-1;
 		else kth= pow(2,height) - lastLevelLeaves-1;
 		buildingLeaves(kth,numberNodes,1);
 		if(prevLevelLeaves > 0){
@@ -151,13 +86,13 @@ class RMMTree{
 		node.excessMax = 0 - w;
 		node.excessMin = w;
 		node.numberExcessMin = 0;
-		
+
 		for(int i=0; i < (1<<w); i++){
 			tableC.push_back(node);
 
 			for(int j=0; j<w;j++){
 				tableC[i].excess += i & (1 << (w-j-1)) ? 1 : -1;
-				
+
 				if(tableC[i].excess > tableC[i].excessMax){
 					tableC[i].excessMax = tableC[i].excess;
 				}
@@ -181,7 +116,7 @@ class RMMTree{
 			tree[i].numberExcessMin =0;
 			for(int p = ((k-1)*sizeBlock/w)+1;p<=k*sizeBlock/w;p++){
 			 	x = bitsread((p-1)*w,((p-1)*w)+w-1);
-				
+
 				if(tree[i].excess + tableC[x].excessMax > tree[i].excessMax){
 					tree[i].excessMax = tree[i].excess + tableC[x].excessMax;
 				}
@@ -192,7 +127,7 @@ class RMMTree{
 					tree[i].numberExcessMin = tree[i].numberExcessMin + tableC[x].numberExcessMin;
 				}
 				tree[i].excess += tableC[x].excess;
-			} 
+			}
 		}
 
 	}
@@ -216,7 +151,7 @@ class RMMTree{
 				tree[i].numberExcessMin = tree[vR].numberExcessMin;
 			}else{
 				tree[i].excessMin = tree[vL].excessMin;
-				tree[i].numberExcessMin = tree[vL].numberExcessMin + tree[vR].numberExcessMin; 
+				tree[i].numberExcessMin = tree[vL].numberExcessMin + tree[vR].numberExcessMin;
 			}
 		}
 	}
@@ -235,14 +170,14 @@ class RMMTree{
 		printNode(tree, 0);
 		cout << " ----- Internal nodes ----- \n";
 		for(i=1;i<numberNodes-numberLeaves;i++)printNode(tree, i);
-		
+
 		cout << " ----- Folhas -----" << endl;
 		for(;i<numberNodes;i++)printNode(tree,i);
 	}
 
 	void printTableC(){
 		cout << "---- Tabela C, para acelerar a construção da RMM-tree ----" << "\n\n";
-		for(int i=0;i<tableC.size();i++)printNode(tableC,i);
+		for(int i=0;i<pow(2,w);i++)printNode(tableC,i);
 	}
 
 	void printInfoTree(){
@@ -260,9 +195,7 @@ int main(){
 	int_vector<1> v = {1,1,1,0,1,0,0,1,1,1,1,1,0,1,0,1,0,0,0,1,0,0,1,1,1,0,1,1,0,0,1,0,1,0,0,0,0,1,0,0};
 	RMMTree t(v,4);
 	t.buildingTree();
-	int r = t.fwdSearch(2,-1);
-	cout << "resposta=" << r <<"\n";
 	//t.printTree();
-	//t.printTableC();
+	t.printTableC();
 	return 0;
 }
