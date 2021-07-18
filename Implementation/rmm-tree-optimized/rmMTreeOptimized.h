338ee053e6e7552ce60ad5e42697cbaa1b3d3449
Danyelle Angelo <danyangelofsa@gmail.com>
2021-07-18 16:17:35 -0300

    Criando estrutura otimizada.Operaçoes: bwdsearch,fwdsearch,findopen,findclose

---
+++ Implementation/rmm-tree-optimized/rmMTreeOptimized.h
@@ -0,0 +1,241 @@
+#ifndef RMMTREEO_H
+#define RMMTREEO_H
+
+#include <sdsl/int_vector.hpp>
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/bp_support_sada.hpp>
+#include <vector>
+
+using namespace sdsl;
+using std::vector;
+
+typedef struct Key{
+    int excess;
+    int excessMax;
+    int excessMin;
+    int numberExcessMin;
+}Key;
+
+typedef struct Node{
+    int nKeys;
+    vector<Key> keys;
+}Node;
+
+class RMMTree{
+    public:
+        bit_vector bv;	// Vetor de bits que armazena a sequência de parênteses balanceados
+
+         /*!
+		*	@brief Construtor
+		*/
+        RMMTree(int_vector<1> &bv, int sizeBlock, int w, int order);
+
+        //métodos públicos 
+
+		unsigned long long cLog_m(unsigned long long  n,unsigned long long  m);
+		unsigned long long fLog_m(unsigned long long  n,unsigned long long  m);
+		/*!
+		*	@brief ler os bits de s até e de bv, e contabiliza o inteiro correspondente a esta sequência binária
+		*	@param s: ponto de partida da leitura
+		*	@param e: ponto final da leitura
+		*	@return inteiro correspondente aos bits lidos.
+		*/
+		int bitsread(int s,int e);
+	
+		/*!
+		*	@brief verifica se a k-th folha está no último ou penúltimo nível da árvore e calcula sua posição.
+		*	A ordem das folhas aqui vai de 0 à numberLeaves-1.
+		*	@param k = k-th folha
+		*	@return índice da k-th folha na rmM-tree
+		*/
+		int leafInTree(int k);
+
+		/*!
+		*	@brief Dado um índice na rmM-tree,verifica o nível em que a folha está, e calcula a sua ordem.
+		*	A ordem das folhas vai de 0 à numberLeaves-1
+		*	@param v : índice da folha na rmM-tree
+		*	@return ordem da folha
+		*/
+		int numLeaf(int v);
+
+		/*!
+		*	@brief Dado a k-th folha da rmM-tree e um índice i em bp, calcula o ínicio do bloco da k-th folha
+		*	e então computa a que chave i pertence.
+		*	@param k : k-th folha da rmM-tree
+		*	@param i: índice em bp
+		*	@return chave em v onde i está localizado.
+		*/
+		int numKey(int k, int i);
+
+		/*!
+		*	@brief constrói a estrutura da rmM-tree, chamando primeiro a função para pré-computar
+		*	uma tabela que irá acelerar o processo, e chamando depois a função para construir as folhas
+		*	da rmM-tree e por último seus nós internos.
+		*/
+		void buildingTree();
+
+		/*!
+		*	@brief Realiza um percusor de subida e depois desciada na rmM-tree,  nos nós a direita
+		* 	da folha (e na própria folha)  que contém a posição i+1 em busca do excesso d.
+		*	@param i: Posição a partir da qual quero realizar a busca (será incrementado de 1)
+		*	@param d: excesso de "1" buscado (profundidade)
+		*	@return primeira posição j > i em bv onde ocorre o excesso d.
+		*/
+		int fwdSearch(int i,int d);
+
+		/*!
+		*	@brief Realiza um percusor de subida e depois desciada na rmM-tree através dos nos nós 
+		* 	a esquerda da folha (e na própria folha)  que contém a posição i em busca do excesso d.
+		*	@param i: Posição a partir da qual quero realizar a busca.
+		*	@param d: excesso de "1" buscado (profundidade)
+		*	@return primeira posição j < i em bv onde ocorre o excesso d.
+		*/
+		int bwdSearch(int i,int d);
+
+		/*!
+		*	@brief busca o parênteses de fechamento, corresponde ao parênteses  de abertura i.
+		*	para isso, busca através da função fwdSearch() a primeira posição j >i onde ocorre
+		*	o excesso -1.
+		*	@param i: índice de um parênteses de abertura em bv, que cofica um nó
+		*	@return índice j do parênteses de fechamento corresponde ao parênteses de abertura i, ou i, caso
+		*	i codifique um parênteses de fechamento.
+		*/
+		int findClose(int i);
+
+		/*!
+		*	@brief busca o parênteses de abertura, corresponde ao parênteses  de fechamento i.
+		*	para isso, busca através da função bwdSearch() a primeira posição j < i onde ocorre
+		*	o excesso 0.
+		*	@param i: índice de um parênteses de fechamento em bv
+		*	@return índice j do parênteses de abertura corresponde ao parênteses de fechamento i, ou i, caso
+		*	i codifique um parênteses de abertura.
+		*/		
+		int findOpen(int i);
+		
+		/*!
+		*	@brief Imprime os valores da tabela de aceleração através da função printNode()
+		*/
+		void printTableC();
+
+		/*!
+		*	@brief Imprime a raíz, depois nós internos e por último folhas da rmM-tree através da 
+		*	função printNode()
+		*/
+		void printTree();
+
+		/*!
+		*	@brief Imprime as informações básicas da rmM-tree, número de nós, número de folhas, altura
+		*	e tamanho de cobertura de bloco.
+		*/
+		void printInfoTree();
+
+
+    private:
+		rank_support_v<1> b_rank1;			// Fornece suporte a operaçãop  rank, tendo como alvo bit 1
+		rank_support_v<0> b_rank0;			// Fornece suporte a operaçãop  rank, tendo como alvo bit 0
+		rank_support_v<10,2> b_rank10;		// Fornece suporte a operaçãop  rank, tendo como alvo a ocorrência do bit 1,seguido do bit 0
+		select_support_mcl<1> b_sel1;		// Fornece suporte a operaçãop  select, tendo como alvo bit 1
+		select_support_mcl<0> b_sel0;		// Fornece suporte a operaçãop  select, tendo como alvo bit 0
+		select_support_mcl<10,2> b_sel10;	// Fornece suporte a operaçãop  select, tendo como alvo a ocorrência do bit 1,seguido do bit 0
+		int sizeBlock;						// Tamanho do intervalocoberto por um nó folha
+		int w;								// Divisor de sizeBlock. usado para pecorrer os bits de bv, de w em w, e assim acelerar o processo
+		int size;							// Tamanho da sequência de parênteses balanceados.
+		int numberLeaves;					// Quantidade de folhas na rmM-tree
+		int numberNodes;					// Número de nós da rmM-tree
+		int order;                          // Ordem da rmM-tree
+        int height;							// Altura da rmM-tree
+		vector<Node> tree;					// Vetor do tipo Node, usado para armazenar a Range-min-max tree
+		vector<Key> tableC;				// Tabela de bits, com valores de excesso pré-computados,usados para acelar a construção da rmM-tree
+
+        //métodos privados
+		/*!
+		*	@return retorna a se a < b e b se b <=a. 
+		*/
+		int min(int a , int b);
+
+		/*!
+		*	@brief Pré-computa uma tabela de excessos C, para agilizar a construção e as operações da RMM-tree.
+		*/
+		void buildingTableC();
+
+		/*!
+		*	@brief Constroí as folhas de cada nível da RmM-tree.
+		*/
+		void buildingLeaves();
+		
+		/*!
+		*	@brief Constroí os nós internos e a raíz da RmM-tree
+		*/
+		void buildingInternalNodesRoot();
+
+		/*!
+		*	@brief Pecorre as chaves da folha v (a qual i pertence) mais a esquerda de i.
+		*	e executa a varredura bloco a bloco procurando pelo excesso d.
+		*	@param i: Posição a partir da qual devo buscar o excesso.
+		*	@param nKeys: Número de chaves na folha v.
+		*	@param d: Excesso buscado
+		*	@param dr: Excesso relativo (atualizado a cada posição que avançamos no bloco)
+		*	@return a posição em que ocorre o excesso d ou bv.size() caso o excesso não se encontre neste bloco.
+		*/
+		int fwdKey(int i,int key,int k,int nKeys,int d,int &dr);
+
+		/*!
+		*	@brief Pecorre para frente cada subbloco de tamanho "w" do bloco pertencente à "i".
+		*	@param i: Posição a partir da qual devo buscar o excesso (i é adicionado de 1)
+		*	@param d: Excesso buscado
+		*	@param dr: Excesso relativo (atualizado a cada posição que avançamos no bloco)
+		*	@return a posição em que ocorre o excesso d ou bv.size() caso o excesso não se encontre neste bloco.
+		*/
+		int fwdBlock(int i,int d,int &dr);
+
+		/*!
+		*	@brief Calcula o pai do nó v, obtém o número de filhos deste nó, e a partir daí
+		*	verifica se os irmãos mais à esquerda de v, compreendem o excesso d nos seus intervalos de
+		*	máximo e mínimo.
+		*	@param v: nó da rmM-tree investigado na passagem pela árvore
+		*	@param dr: excesso relativo dr
+		*	@param d: excesso procurado
+		*	@return a chave que contém o excesso d desejado ou size caso a resposta não seja encontrada nos irmãos de v.
+		*/
+		int fwdVerifySibling(int &v,int &dr, int d);
+
+		/*!
+		*	@brief Pecorre as chaves da folha v (a qual i pertence) mais a direita de i.
+		*	e executa a varredura bloco a bloco procurando pelo excesso d.
+		*	@param i: Posição a partir da qual devo buscar o excesso.
+		*	@param d: Excesso buscado
+		*	@param dr: Excesso relativo (atualizado a cada posição que avançamos no bloco)
+		*	@return a posição em que ocorre o excesso d ou -1 caso o excesso não se encontre neste bloco.
+		*/
+		int bwdKey(int i,int key,int k,int d,int &dr);
+
+		/*!
+		*	@brief Pecorre para trás cada subbloco de tamanho "w" do bloco pertencente à "i".
+		*	@param i: Posição a partir da qual devo iniciar a busca para trás do excesso
+		*	@param d: Excesso buscado
+		*	@param dr: Excesso relativo (atualizado a cada posição que avançamos no bloco)
+		*	@return a posição (subtraída de 1) onde ocorre o excesso d ou ou -1 caso o excesso não se encontre neste bloco.
+		*/
+		int bwdBlock(int i,int d,int &dr);
+
+		/*!
+		*	@brief Calcula o pai do nó v, obtém o número de filhos deste nó, e a partir daí
+		*	verifica se os irmãos mais à direita de v, compreendem o excesso d nos seus intervalos de
+		*	máximo e mínimo.
+		*	@param v: nó da rmM-tree investigado na passagem pela árvore
+		*	@param dr: excesso relativo dr
+		*	@param d: excesso procurado
+		*	@return a chave que contém o excesso d desejado, e -1 caso a resposta não seja encontrada nos irmãos de v.
+		*/
+		int bwdVerifySibling(int &v,int &dr, int d);
+
+
+		/*!
+		*	@brief Imprime as informações de excesso de uma chave ou elemento de tabela
+		*	@param vector: estrutura (árvore ou tabela) que terá seu nó impresso
+		*	@param i: índice do elemento da estrutura.
+		*/
+		void printNode(vector<Key> vector, int i);
+};
+
+#endif
---
+++ Implementation/rmm-tree-optimized/rmM_tree_optimized.cpp
@@ -0,0 +1,434 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <math.h>
+#include <sdsl/int_vector.hpp>
+#include <sdsl/bp_support_sada.hpp>
+#include "rmMTreeOptimized.h"
+
+using namespace std;
+using namespace sdsl;
+//https://github.com/google/benchmark para monitorar o tempo
+int temp;
+
+RMMTree::RMMTree(int_vector<1> &bv, int sizeBlock, int w, int order){
+	assert(!(order&(order-1)));
+	this->bv = bv;
+	this->b_rank1 = rank_support_v<1>(&bv);
+	this->b_rank0 = rank_support_v<0>(&bv);
+	this->b_rank10 = rank_support_v<10,2>(&bv);
+	this->b_sel1 = select_support_mcl<1>(&bv);
+	this->b_sel0 = select_support_mcl<0>(&bv);
+	this->b_sel10 = select_support_mcl<10,2>(&bv);
+	this->sizeBlock = sizeBlock;
+	this->w =  w;
+	this->size = bv.size();
+    this->order = order;
+	this->numberLeaves = ceil( (double)bv.size()/(sizeBlock*order) );
+	this->numberNodes = ceil((double)(order*numberLeaves-1)/(order-1));
+	this->height = cLog_m(this->numberLeaves,this->order);
+	this->tree.resize(this->numberNodes);
+	temp = sqrt(order);
+}
+
+unsigned long long RMMTree::cLog_m(unsigned long long  n,unsigned long long  m){
+	return ceil(log2(n)/log2(m));
+}
+
+unsigned long long RMMTree::fLog_m(unsigned long long  n,unsigned long long  m){
+	return floor(log2(n)/log2(m));
+}
+
+int RMMTree::min(int a , int b){
+	return (a < b )? a:b;
+}
+
+int RMMTree::bitsread(int s,int e){
+	int value=0;
+	for(int j=s;j<=e;j++)value = (value << 1) + bv[j] ;
+	return value;
+}
+
+
+int RMMTree::leafInTree(int k){
+	//se number Leaves é potência de order, todas as folhas estão no mesmo nível
+	int nNodesPrevLevel = 1 << (temp*(height-1));
+	int totalNodesExcludingLastLevel = ceil((double)(order*nNodesPrevLevel -1)/(order-1));
+	
+	if(k < numberNodes -totalNodesExcludingLastLevel){//último nível
+		return totalNodesExcludingLastLevel+k; 
+	}
+	return totalNodesExcludingLastLevel  - numberLeaves + k;
+}
+
+int RMMTree::numLeaf(int v){
+	//se number Leaves é potência de order, todas as folhas estão no mesmo nível
+	int nNodesPrevLevel = 1 << (temp*(height-1));
+	int totalNodesExcludingLastLevel = (order*nNodesPrevLevel -1)/(order-1);
+
+
+	if(v >= totalNodesExcludingLastLevel){
+		return v - totalNodesExcludingLastLevel;
+	}
+	return v - totalNodesExcludingLastLevel + numberLeaves;
+}
+
+
+int RMMTree::numKey(int k,int i){
+	int startBlock= k*sizeBlock*order;
+	return floor((i-startBlock)/order);
+}
+
+void RMMTree::buildingTree(){
+    /*pré-computa tabela C, para acelerar a cosntruçã da RMM-tree*/
+	buildingTableC();
+
+	/*constrói as folhas da árvore*/
+	buildingLeaves();
+	/*Inicia a construção dos nós internos e raíz*/
+	buildingInternalNodesRoot();
+}
+
+void RMMTree::buildingTableC(){
+	Key key;
+	key.excess = 0;
+	key.excessMax = 0 - w;
+	key.excessMin = w;
+	key.numberExcessMin = 0;
+
+	for(int i=0; i < (1<<w); i++){
+		tableC.push_back(key);
+
+		for(int j=0; j<w;j++){
+			tableC[i].excess += i & (1 << (w-j-1)) ? 1 : -1;
+
+			if(tableC[i].excess > tableC[i].excessMax){
+				tableC[i].excessMax = tableC[i].excess;
+			}
+			if(tableC[i].excess < tableC[i].excessMin){
+				tableC[i].excessMin = tableC[i].excess;
+				tableC[i].numberExcessMin = 1;
+			}else if(tableC[i].excess == tableC[i].excessMin){
+				tableC[i].numberExcessMin+=1;
+			}
+		}
+    }
+}
+
+void RMMTree::buildingLeaves(){
+	int x,v, numKey=0;
+	Key key;
+	for(int k=0; k < numberLeaves;k++){
+		v = leafInTree(k);
+
+		while (tree[v].nKeys < order && numKey < ceil(size/sizeBlock)){
+			key.excess = 0;
+			key.excessMax = 0 -w;
+			key.excessMin = 0 +w;
+			key.numberExcessMin = 0;
+			for(int p = (numKey*(sizeBlock/w))+1;p<=((numKey+1)*sizeBlock)/w;p++){
+				x = bitsread((p-1)*w,(p*w)-1);
+				if(key.excess + tableC[x].excessMax > key.excessMax){
+					key.excessMax = key.excess + tableC[x].excessMax;
+				}
+				if(key.excess + tableC[x].excessMin < key.excessMin){
+					key.excessMin = key.excess + tableC[x].excessMin;
+					key.numberExcessMin = tableC[x].numberExcessMin;
+				}else if(key.excess + tableC[x].excessMin == key.excessMin){
+					key.numberExcessMin = key.numberExcessMin + tableC[x].numberExcessMin;
+				}
+				key.excess += tableC[x].excess;
+			}
+			tree[v].keys.push_back(key);
+			tree[v].nKeys++;
+			numKey++;
+		}
+	}
+}
+
+void RMMTree::buildingInternalNodesRoot(){
+	Key key;
+	for(int v= numberNodes - numberLeaves -1; v>=0;v--){
+		for(int child=1; child <= order && (v*order+child)<numberNodes;child++){
+			key.excess = 0;
+			key.excessMax = 0 -w;
+			key.excessMin = 0+w;
+			key.numberExcessMin = 0;
+			//define o filho alvo
+			int targetSon = (order*v)+child;
+			
+			//cria laço para pecorrer as chaves do filho alvo
+			for(int i=0; i < tree[targetSon].nKeys;i++){
+				if(key.excessMax < key.excess + tree[targetSon].keys[i].excessMax){
+					key.excessMax = key.excess + tree[targetSon].keys[i].excessMax;
+				}
+				if(key.excessMin > key.excess + tree[targetSon].keys[i].excessMin){
+					key.excessMin = key.excess + tree[targetSon].keys[i].excessMin;
+					key.numberExcessMin = tree[targetSon].keys[i].numberExcessMin;
+				}else if(key.excessMin == key.excess + tree[targetSon].keys[i].excessMin){
+					key.numberExcessMin+= tree[targetSon].keys[i].numberExcessMin;
+				}
+				key.excess += tree[targetSon].keys[i].excess;
+			}
+			tree[v].keys.push_back(key);
+			tree[v].nKeys++;
+		}
+	}
+}
+
+int RMMTree::fwdKey(int i,int key,int k,int nKeys,int d,int &dr){
+	int j;
+	for(;key < nKeys;key++){
+		j= fwdBlock(i,d,dr);
+		if(dr == d) return j;
+
+		i = (order*k+key+1)*sizeBlock -1;//obtém o fim da chave atual, para começar a varrer próxima chave a partir do seu início
+	}
+	return size;
+}
+
+int RMMTree::fwdBlock(int i,int d,int &dr){
+	int p;
+	int fb = ceil((double)(i+1)/w);//para calcular o limite do primeiro bloquinho (de i)
+	int lb = ceil((double)(i+2)/sizeBlock)* (sizeBlock/w);//limite do bloco 
+	
+	//varre o sub-bloco a qual i+1 pertebce
+	for(int j=i+1;j<=(fb*w)-1;j++){
+		dr += (bv[j] == 1)? 1 : -1;
+		if(dr == d)return j;
+	}
+	
+	//Verifica se "d" está contido no bloco subsequente
+	for(p=fb+1; p<=lb;p++){
+		int x = bitsread((p-1)*w, (p*w)-1);
+		if(dr + tableC[x].excessMin <= d && dr + tableC[x].excessMax >= d){
+			break;
+		}
+		dr += tableC[x].excess;
+	}
+	
+	if(p > lb)return lb*sizeBlock;//d não está no bloco subsequente
+
+	//Finalmente faz a varredura do subbloco subsquente ao de i+1, onde se encontra d
+	for(int j= (p-1)*w; j <= (p*w)-1;j++){
+		dr += (bv[j] ==1)? 1:-1;
+		if(dr == d)return j;
+	}
+    return size;
+}
+
+int RMMTree::fwdVerifySibling(int &v, int &dr, int d){
+	
+	//calcula parent a fim de verificar quantos írmãos de v existem a sua esquerda
+	int parent = floor((double)(v-1)/order);
+	int child = v - (parent*order);//obtém o número de irmãos a direita de v
+	v++;
+	
+	//pecorre os írmãos de v
+	for( ;child < tree[parent].nKeys && v<numberNodes;child++){
+		
+		for(int key=0;key<tree[v].nKeys;key++){
+			
+			if((dr + tree[v].keys[key].excessMin <= d) && (d<= dr +tree[v].keys[key].excessMax))return key;
+			dr +=  tree[v].keys[key].excess;
+		}
+		v++;
+	}
+	
+	return size;//a resposta não está nos irmãos de v
+}
+
+int RMMTree::fwdSearch(int i,int d){
+	assert((i+1)>=0 && (i+1)< size);
+
+	int dr=0;
+	int k = floor((double)(i+1)/(sizeBlock*order));//calcula a k-th folha em que se encontra i+1
+	int v = leafInTree(k);//índice da RMM-tree onde ocorre a k-th folha
+	int key = numKey(k,i+1); 
+	
+	int j = fwdKey(i,key, k ,tree[v].nKeys,d,dr);
+	if(dr == d)return j;
+	
+	/* -----Subindo a RMM-tree ------*/
+	while( v!=0 && (key=fwdVerifySibling(v,dr,d))==size){
+		v = floor((double)(v-1)/order);
+	} 
+	
+	if(v < numberNodes - numberLeaves)v = (v*order)+1+key;
+	
+	/* ----- Descendo a RMM-tree ------*/
+	while(v < numberNodes - numberLeaves){
+		/*pecorre todas achavaes do nó pelo qual estamos descendo, para encontrar a chave em que ocorre
+		o excesso e descer pelo o seu nó.
+		*/
+		for(key=0;key<tree[v].nKeys;key++){
+			if((dr+tree[v].keys[key].excessMin <=d)&& (dr + tree[v].keys[key].excessMax >=d) ){
+				v = (v*order)+1+key;
+				break;
+			}
+			else{
+				dr += tree[v].keys[key].excess;
+			}
+		}
+	}
+ 
+	k = numLeaf(v);
+	
+	j = fwdKey((order*k+key)*sizeBlock -1,0, k ,tree[v].nKeys,d,dr);
+
+	return (dr == d)? j : size;
+}
+
+int RMMTree::bwdKey(int i,int key, int k,int d,int &dr){
+	int j;
+	
+	for(; key>=0;key--){
+		j = bwdBlock(i,d,dr);
+		if(dr==d)return j;
+		i =  (order*k+key)*sizeBlock -1;//obtém o inicio da chave atual subtraído de 1, para começar a varrer a próxima chave a partir do seu final
+	}
+	return -1;
+}
+
+int RMMTree::bwdBlock(int i,int d,int &dr){
+	int p,x,j;
+	int fb = floor((double)i/w);
+	int lb = (floor((double)i/sizeBlock) * (sizeBlock/w));
+	
+	for(j=i;j>=fb*w;j--){
+		dr += (bv[j] == 1)? -1 : 1;
+		if(dr == d)return j-1;
+	}
+	
+	for(p=fb-1;p>=lb;p--){
+		x = bitsread((p*w),(p+1)*w-1);
+		if( (dr - tableC[x].excess + tableC[x].excessMin <= d)&& (dr - tableC[x].excess + tableC[x].excessMax >= d) ){
+			break;
+		}
+		dr -= tableC[x].excess;
+	}
+
+	if(p < lb)return (lb*w)-1;
+
+	for(j=(p+1)*w-1; j>=p*w; j--){
+		dr += (bv[j] == 1)? -1 : 1;
+		if(dr == d)return j-1;
+	}
+	
+	return size;
+}
+
+int RMMTree::bwdVerifySibling(int &v, int &dr, int d){
+	int parent = floor((double)(v-1)/order);
+	int child = v - (parent*order)-1; // obtém o número de irmãos a esquerda de v
+
+	v--;
+
+	for(; child >0 && v >0;child--){
+		for(int key =tree[v].nKeys-1; key>=0;key--){
+			if((dr - tree[v].keys[key].excess + tree[v].keys[key].excessMin <= d) && (d <= dr - tree[v].keys[key].excess + tree[v].keys[key].excessMax))return key;
+			dr-=tree[v].keys[key].excess;
+		}
+		v--;
+	}
+	return -1;
+}
+
+int RMMTree::bwdSearch(int i,int d){
+	assert(i>=0 && i < size);
+	if(i==0)return -1;
+
+	int dr=0;
+	int k = floor((double)i/(sizeBlock*order));
+	int v = leafInTree(k);
+	int key = numKey(k,i);
+
+	int j = bwdKey(i,key,k,d,dr);
+	if(dr==d) return j;
+	
+	/* -----Subindo a RMM-tree ------*/
+	while(v!=0 && (key=bwdVerifySibling(v,dr,d))==-1){
+		v = floor((double)(v-1)/order);
+	}
+	
+	if(v < numberNodes - numberLeaves)v = (v*order)+1+key;
+	
+	/* ----- Descendo a RMM-tree ------*/
+	while(v < numberNodes - numberLeaves){
+		for(key = tree[v].nKeys;key>=0;key--){
+			if( (dr - tree[v].keys[key].excess +tree[v].keys[key].excessMin <= d)&&(dr - tree[v].keys[key].excess +tree[v].keys[key].excessMax >= d)){
+				v = (v*order)+1+key;
+				break;
+			}
+			else{
+				dr -= tree[v].keys[key].excess;
+			}
+		}
+	}
+	
+	k = numLeaf(v);
+	if(dr == d)return (k+key+1)*sizeBlock-1;
+	
+	j = bwdKey(((k+1)*tree[v].nKeys)*sizeBlock-1,tree[v].nKeys-1,k,d,dr);
+
+	return (dr==d)? j : -1;
+}
+
+
+int RMMTree::findClose(int i){
+	assert(i>=0 && i<size);
+
+	if((i == 0) && bv[i]==1)return size -1;
+	return (bv[i] == 0) ? i : fwdSearch(i,-1);
+}
+
+int RMMTree::findOpen(int i){
+	assert(i>=0 && i < size);
+
+	if((i == (int)(size-1)) && bv[i]==0)return 0;
+	return (bv[i] == 1) ?  i : bwdSearch(i,0)+1;
+}
+
+void RMMTree::printNode(vector<Key> vector, int i){
+	cout << "v[" << i<< "].e = " << vector[i].excess << "; "
+		 << "v[" << i<< "].m = " << vector[i].excessMin << "; "
+		 << "v[" << i<< "].M = " << vector[i].excessMax << "; "
+	     << "v[" << i << "].n = " << vector[i].numberExcessMin << ".\n"
+		 << endl;
+}
+
+void RMMTree::printTableC(){
+	cout << "---- Tabela C, para acelerar a construção da RMM-tree ----" << "\n\n";
+	for(int i=0;i<(1 << w);i++)printNode(tableC,i);
+}
+
+void RMMTree::printTree(){
+	int v;
+	cout << " ----- Root and Internal nodes ----- \n";
+	for(v=0;v<numberNodes-numberLeaves;v++){
+		cout << " Nó " << v << "\n";
+		for(int k=0;k<tree[v].nKeys; k++){
+			cout << "Chave " << k << "\n";
+			printNode(tree[v].keys , k);
+		}
+	}
+	cout << " ----- Folhas -----" << endl;
+	for(int k=0;v<numberNodes;v++,k++){
+		cout << numLeaf(v) << "-th folha " << " - nó " << v << "\n";
+		for(int k=0;k<tree[v].nKeys; k++){
+			cout << "Chave " << k << "\n";
+			printNode(tree[v].keys , k);
+		}
+	}
+}
+
+void RMMTree::printInfoTree(){
+    cout << "BPS: " << this->bv << '\n'
+		 << "Tamanho de bloco: " << sizeBlock << '\n'
+		 << "Ordem da árvore " << order << '\n'
+		 << "Quantidade de folhas: " << numberLeaves << '\n'
+		 << "Quantidade de nós: " << numberNodes << '\n'
+		 << "Altura da árvore: " << height << '\n'
+		 << endl;
+}
+
