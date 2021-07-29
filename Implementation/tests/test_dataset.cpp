c8c75a9480e2838e11a5e232ed488da032d14c14
Danyelle Angelo <danyangelofsa@gmail.com>
2021-07-29 00:11:16 +0000

    Tetando uma nova forma de usar o benchmark

--- Implementation/tests/test_dataset.cpp
+++ Implementation/tests/test_dataset.cpp
@@ -22,6 +22,7 @@ class RMMTreeFixtureTest : public ::testing::Test{
 	    t = new RMMTree(v,sizeBlock,w); 
 	    bps = new bp_support_sada<>(&(t->bv)); 
             size = (int)v.size(); t->buildingTree();
+	    srand(size);
         }
         void TearDown(){
 	    delete t;
@@ -30,18 +31,19 @@ class RMMTreeFixtureTest : public ::testing::Test{
 };
 
 TEST_F(RMMTreeFixtureTest, fwdSearch_findClose){
-	int index[] = {0,1,3,4,7,8,9,10,11,15,13,18,19,20,22,23,24,26,27,30,32};
-	for(int i=0;i<(int)(sizeof(index)/sizeof(index[0]));i++){
-            EXPECT_EQ(t->findClose(index[i]),bps->find_close(index[i])) << "Resposta errada ao calcular o find_close de i=" << index[i];
+	for(int i=0;i<20;i++){
+	    int k = rand()%size;
+	    if(v[k]==1)
+            	EXPECT_EQ(t->findClose(k),bps->find_close(k)) << "Resposta errada ao calcular o find_close de i=" << k;
     	} 
 }
 
 TEST_F(RMMTreeFixtureTest, bwdSearch_findOpen){
-	int index[] =  {3,5,6,12,14,16,17,18,20,21,25,28,29,31,33,34,35,36,38,39};
 
-	for(int i=0;i<(int)(sizeof(index)/sizeof(index[0]));i++){ 
-
-        	EXPECT_EQ(t->findOpen(index[i]),bps->find_open(index[i])) << "Resposta errada ao calcular o find_open de i=" << index[i];
+	for(int i=0;i<20;i++){ 
+		int k = rand()%size;
+		if(v[k]==0)
+        	    EXPECT_EQ(t->findOpen(k),bps->find_open(k)) << "Resposta errada ao calcular o find_open de i=" << k;
    	 } 
 }
 
--- Implementation/tests/test_kary_rmm_tree.cpp
+++ Implementation/tests/test_kary_rmm_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <benchmark/benchmark.h>
 #include <sdsl/int_vector.hpp>
 #include <sdsl/bp_support_sada.hpp>
@@ -9,59 +10,73 @@ using namespace sdsl;
 using namespace std;
 
 RMMTree *t;
-int size;
 
 class Kary_RMMTree_FixtureBM: public benchmark::Fixture{
 	public:
 		int sizeBlock =8;
 		int w=4;
+		int order=32;
+		int size;
 		int_vector<1> v;
-		int order=64;
-	Kary_RMMTree_FixtureBM(){
-		parentheses_to_bits("wiki.par",v);
+		vector<int> input_close; 
+		vector<int> input_open;
+
+	void SetUp(){
+		//parentheses_to_bits("wiki.par",v);
+		size = v.size();
 		t = new RMMTree(v,sizeBlock,w,order);
-		size =(int) v.size();		
 		t->buildingTree();
+		argumentsFindClose(10,size);
+		argumentsFindOpen(10,size);
 		srand(size);
+		cout << "oiii\n";
+	}
+	void TearDown(){
+		delete t;
+	}
+
+	void argumentsFindClose(int n,int lim){
+		int i=0, k;
+		while(i<n){
+			k = rand()%lim;
+			if(v[k]==1){
+				input_close.push_back(k);
+				i++;
+			}
+		}
+	}
+
+	void argumentsFindOpen(int n,int lim){
+		int i=0, k;
+		while(i<n){
+			k = rand()%lim;
+			if(v[k]==0){
+				input_open.push_back(k);
+				i++;
+			}
+		}
 	}
 };
 
-static void ArgumentsFindClose(benchmark::internal::Benchmark *v){
-	int k,i=0;
-	while(i<200){
-		k = rand()%size;
-		if(t->bv[k]==1){
-			v->Args({k});
-			i++;
-		}
-	}
-}
-
-static void ArgumentsFindOpen(benchmark::internal::Benchmark *v){
-	int k,i=0;
-	while(i<200){
-		k = rand()%size;
-		if(t->bv[k]==0){
-			v->Args({k});
-			i++;
-		}
-	}
-}
-
 BENCHMARK_DEFINE_F(Kary_RMMTree_FixtureBM, findClose_kary)(benchmark::State& st){
 	for(auto _ :st){
-		t->findClose(st.range(0));
+		for(int i=0;i<10;i++){
+			t->findClose(input_close[i]);
+		}
 	}
 }
-BENCHMARK_REGISTER_F(Kary_RMMTree_FixtureBM,findClose_kary)->Apply(ArgumentsFindClose);
+BENCHMARK_REGISTER_F(Kary_RMMTree_FixtureBM,findClose_kary);
 
 
 BENCHMARK_DEFINE_F(Kary_RMMTree_FixtureBM, findOpen_kary)(benchmark::State& st){
 	for(auto _ :st){
-		t->findOpen(st.range(0));
+		for(int i=0;i<10;i++){
+			t->findOpen(input_open[i]);
+		}
 	}
 }
-BENCHMARK_REGISTER_F(Kary_RMMTree_FixtureBM,findOpen_kary)->Apply(ArgumentsFindOpen);
+BENCHMARK_REGISTER_F(Kary_RMMTree_FixtureBM,findOpen_kary);
+
 
 int main(int argc, char **argv){
 	benchmark::Initialize(&argc,argv); 
