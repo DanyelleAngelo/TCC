79b6d5b02e1c0cac1352aa21c63c9d6c18ae55e8
danyelleangelo <danyangelofsa@gmail.com>
2021-10-26 18:40:52 -0300

    Gerando novo profile

--- Implementation/profile/profile_fwdSearch.cpp
+++ Implementation/profile/profile_fwdSearch.cpp
@@ -11,25 +11,12 @@
 using namespace std;
 using namespace sdsl;
 
-int iterations = 25000;
+int iterations = 10000;
 int sizeBlock=32;
 int w=16;
 int size =0;
 RMMTree_Bin *tBin;
 RMMTree_Kary *tKary;
-
-int calc(int a){
-	for(int i=0; i < 100000000;i++){
-		a++;
-	}
-	return a;
-}
-int calc2(int a){
-	for(int i=0; i < 100000000;i+=2){ 
-		a++;
-	}
-	return a;
-}
 
 void generateArguments(vector<int> &vArgs, vector<int> &d){
     srand(size/8);
@@ -62,7 +49,7 @@ void fwdSearch_kary_rmMTree(vector<int> vArgs, vector<int> d){
 int main(){
     vector<int> vArgs, d;
     int_vector<1> v;
-    parentheses_to_bits("data.txt", v);
+    parentheses_to_bits("../dataset/dna.par", v);
     size = v.size();
 
     /*building binary rmM-tree*/
@@ -79,7 +66,7 @@ int main(){
 
     /*Inicia o profile*/
     cout<< "--------------------Iniciando o profile de fwdSearch para a estrutura binária."<<endl;
-    ProfilerStart("fwdSearch_binary.prof");
+    ProfilerStart("profile_fwdSearch.prof");
     fwdSearch_binary_rmMTree(vArgs,d);
 
     cout<< "--------------------Iniciando o profile de fwdSearch para a estrutura k-ária."<<endl;
