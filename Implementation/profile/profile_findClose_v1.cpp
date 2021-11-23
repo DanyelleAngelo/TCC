e7a8028ef9af51dc80f10698fc218b5499f84b26
danyelleangelo <danyangelofsa@gmail.com>
2021-11-23 10:35:39 -0300

    Profile - Estrutura k-aria visita somente o pai do no

---
+++ Implementation/profile/profile_findClose_v1.cpp
@@ -0,0 +1,76 @@
+#include "../rmm-tree-classic/rmMTreeClassic.h"
+#include "../rmm-tree-optimized/v1.h"
+#include "../read_bp/read_bp.h"
+
+#include <iostream>
+#include <vector>
+#include <sdsl/int_vector.hpp>
+#include <sdsl/bp_support_sada.hpp>
+#include<gperftools/profiler.h>
+
+using namespace std;
+using namespace sdsl;
+
+int iterations = 100000;
+int sizeBlock=32;
+int w=16;
+int size =0;
+RMMTree_Bin *tBin;
+RMMTree_Kary_V1 *tKary;
+
+void generateArguments(vector<int> &vArgs){
+    srand(size/32);
+	int k;
+
+    for(int i=0; i < iterations;){
+        k = rand()%(size-2);
+        if(tBin->bv[k]==1){
+            vArgs.push_back(k);
+            i++;
+        }
+    }
+}
+
+void findClose_binary_rmMTree(vector<int> vArgs){
+    int j;
+	for(int i=0;i<iterations;i++){
+        j = tBin->findClose(vArgs[i]);
+	}
+}
+
+void findClose_kary_rmMTree(vector<int> vArgs){
+    int j;
+    for(int i=0;i<iterations;i++){
+        j = tKary->findClose(vArgs[i]);
+    }
+}
+
+int main(){
+    vector<int> vArgs;
+    int_vector<1> v;
+    parentheses_to_bits("../dataset/prot.par", v);
+    //parentheses_to_bits("../dataset/data.txt", v);
+    size = v.size();
+
+    /*building binary rmM-tree*/
+    tBin = new RMMTree_Bin(v,sizeBlock,w);
+    tBin->buildingTree();
+    
+    
+    /*building k-ary rmM-tree*/
+    tKary = new RMMTree_Kary_V1(v,sizeBlock,w,4);
+    tKary->buildingTree();
+
+    /*create arguments*/
+    generateArguments(vArgs);
+
+    /*Inicia o profile*/
+    cout<< "--------------------Iniciando o profile de findClose para a estrutura binária."<<endl;
+    ProfilerStart("profile_findClose_v1.prof");
+    findClose_binary_rmMTree(vArgs);
+
+    cout<< "--------------------Iniciando o profile de findClose para a estrutura k-ária."<<endl;
+    findClose_kary_rmMTree(vArgs);
+	ProfilerStop();
+    return 0;
+}
